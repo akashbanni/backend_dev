2b54e6dc971c45a5b1f1f6325020710f830c77d7
Akash <akashbanni.ab.1357@gmail.com>
2024-02-15 11:50:07 +0530

    python

---
+++ List.py
@@ -0,0 +1,16 @@
+# list_1 = [10,20,30,40,50,60]
+# print(list_1)
+# #Output : [10, 20, 30, 40, 50, 60]
+# list_2 = [70,80,90]
+# list_12=list_1 + list_2
+# print("List after extending: ",list_12)
+
+
+tuple_1 = (10,20,30,40,50,60)
+print("Tuple: ", tuple_1)
+# Output : Tuple:  (10, 20, 30, 40, 50, 60)
+
+tuple_2 = (70,80,90)
+tup_add = tuple_1 + tuple_2
+print("Tuple after adding: ", tup_add)
+print(tuple_1)
---
+++ practice.py
@@ -0,0 +1,21 @@
+# def myfunc(n):
+#   return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+# write a program to sum two number using class
+
+class adder():
+
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        return self.num1 + self.num2
+    
+a = adder(50,40)
+
+print(a.add())
